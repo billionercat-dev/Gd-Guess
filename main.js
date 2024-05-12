a9c70477a60375e9a5d743f400fc952debb0b4d8
billionercat-dev <142187629+billionercat-dev@users.noreply.github.com>
2024-05-12 16:32:01 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 level = Math.floor((Math.random() * 100000000) + 1);
 
 
-data = fetch('https://gdbrowser.com/api/level/${level}')
+data = fetch(`https://gdbrowser.com/api/level/${level}`)
 
 console.log(data)
